8e07ad20fcb8b285a6ffd05a9274ffcb1eba42c4
Vladislav Kononenko <vladislav.kon@computools.com>
2018-11-11 12:55:43 +0200

    release v1

---
+++ vue-swapi/src/components/PeopleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>HELLO WORLD</h1>
+</template>
+
+<script>
+  export default {
+    name: 'PeopleComponent',
+    data() {
+      return {
+        results: {},
+      };
+    },
+    created() {
+      this.fetchPeoples();
+    },
+    methods: {
+      fetchPeoples() {
+        fetch('https://swapi.co/api/people/').then((response) => {
+          return response.json();
+        }).then((j) => {
+          this.results = j.results;
+        });
+      },
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ vue-swapi/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+    el: '#app',
+    router,
+    render: function (h) {
+        return h(App);
+    }
+});
+
+// new Vue({
+//    router,
+//  render: h => h(App),
+// }).$mount('#app')
